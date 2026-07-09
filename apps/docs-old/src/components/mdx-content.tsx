dc42d167f5bbf73973968e6064e0fc569eddef04
Nicolas Chartiot <nchartiot@proton.me>
2026-07-09 17:34:14 -0300

    remove docs-old

--- apps/docs-old/src/components/mdx-content.tsx
+++
@@ -1,9 +0,0 @@
-import type { CompiledDocArtifact } from "@/lib/content/types";
-
-export function MdxContent({ artifact }: { artifact: CompiledDocArtifact }) {
-  return (
-    <article className="prose prose-slate max-w-none prose-headings:scroll-mt-24 prose-a:text-cyan-700">
-      <div dangerouslySetInnerHTML={{ __html: artifact.html }} />
-    </article>
-  );
-}
